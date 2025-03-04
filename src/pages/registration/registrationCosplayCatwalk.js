06108ce7d281b3b34637e81eef1dcaa7f78d062c
Markus <markus.sibbe@t-online.de>
2025-03-04 22:30:47 +0100

    initial commit

---
+++ src/pages/registration/registrationCosplayCatwalk.js
@@ -0,0 +1,25 @@
+/*
+-Input Felder: Name, Nachname, Künstlername, Name des Charakters/Cosplays
+-Upload Feld: Abgabe Crafting Tagebuch (PDF 2-6 DIN A4 Seiten) im PDF-Format
+-Download der Teilnahmebedingungen
+
+______________________________________________
+Als deko gerne die Crafting Hiru
+Falls dir noch mehr Felder einfallen, die wichtig wären, gerne Ergänzen
+
+
+
+
+Bei unserem Cosplay Catwalk könnt ihr in Kürze die tollsten Cosplays bewundern! Hier hat jeder die Chance, für ein paar Sekunden die Bühne zu erobern und sein Kostüm zu präsentieren. Dabei spielt es keine Rolle, was ihr cosplayt oder woher ihr kommt. Das Craften des Cosplays an sich steht hier im Mittelpunkt. Selbstverständlich haben alle Teilnehmer auch hier die Möglichkeit, etwas zu gewinnen!
+
+
+___________________
+Gerne die Crafting Hiru (Workshop Hiru) als Bild verwenden, oder ein anderes, wenn Ihr eine coolere Idde habt
+Link zu den teilnahmebdingungen / Anmeldeformular auf der Webseite
+
+Jury:
+-Korriban 
+-Dokyato 
+-Eralia  
+-Mineke 
+*/
