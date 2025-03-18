dd0e03f4be4699ce3b1c9e9d9050c8595fdf6c52
Markus <markus.sibbe@t-online.de>
2025-03-19 00:28:59 +0100

    add first text

---
+++ src/components/articles/2025/AnkundigungCosplayVersteigerung.js
@@ -0,0 +1,36 @@
+/*
+
+Cosplayer-Versteigerung für den guten Zweck!
+Auch dieses Jahr freuen wir uns, auf unserer Convention die beliebte Cosplayer-Versteigerung
+gemeinsam mit CoHeki e.V. (Cosplayer helfen Kindern e.V.) durchzuführen! Hier haben Besucher die
+einzigartige Gelegenheit, für eine Stunde am Samstag Nachmittag einen Cosplayer zu ersteigern und
+dabei gleichzeitig etwas Gutes zu tun.
+Der gesamte Erlös der Auktion geht an den Förderkreis für chronisch nierenkranke Kinder und
+Jugendliche Memmingen e. V. (kurz "Nierenkinder").
+Werde Teil dieser besonderen Aktion!
+Letztes Jahr konnten wir gemeinsam unglaubliche 1.555€ durch die Cosplayer-Versteigerung
+sammeln. Die Spendenübergabe fand bei unserem nächsten Event, der YumeKai - Night live auf der
+Bühne statt, wo die erste Vorsitzende des Vereins, Linda Fremuth, die Spende persönlich
+entgegennehmen durfte. Auch in diesem Jahr hoffen wir auf eine große Beteiligung, um erneut eine
+stolze Summe für den guten Zweck zu sammeln!
+Wie kannst du mitmachen?
+Wenn du als Cosplayer Lust hast, dich für den guten Zweck "ersteigern" zu lassen, und du mindesten
+18 Jahre alt bist, dann melde dich jetzt an! Die Teilnehmer der Versteigerung stellen sich am Samstag
+Nachmittag auf der Bühne vor, und die Besucher haben die Möglichkeit, auf eine Stunde mit ihrem
+Lieblingscharakter zu bieten.
+Es gibt keine Anmeldefrist, wir nehmen Cosplayer an, solange der Platz reicht. Auch vor Ort sind
+Anmeldungen noch möglich, sofern es noch freie Plätz gibt.
+Bewerbung für Cosplayer:
+ANMELDEBUTTON / LINK
+Unterstützung durch Sachspenden
+Letztes Jahr gab es zu jedem ersteigerten Cosplayer noch ein weiteres Goodie, welches durch
+Händler, Künstler und unsere Sponsoren bereitgestellt wurde. Gerne können auch in diesem Jahr
+wieder Sachspenden abgegeben werden, um die Versteigerung zu unterstützen. Falls ihr uns auf diese
+Weise helfen möchtet, meldet euch gerne über unser Kontaktformular (mit dem Bereich „Bühne“) bei
+uns!
+Lasst uns gemeinsam erneut eine große Summe für die Nierenkinder sammeln! Wir freuen uns auf
+euch!
+GERNE DIE BILDER DER COSPLAY VERSTEIGERUNG +
+SPENDENÜBERGABE DAZU PACKEN
+
+*/
