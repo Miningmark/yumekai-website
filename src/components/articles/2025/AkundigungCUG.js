562b56cad45bfdb41b84d49c16ed5c4f6b1012fd
Lynn <lynn.hoffmann@t-online.de>
2025-04-09 20:13:33 +0200

    innitial commit

---
+++ src/components/articles/2025/AkundigungCUG.js
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import { StyledLink, DynamicContent } from "@/components/styledComponents";
+
+//Images
+import CUG_LogoImage from "/public/assets/images/yumekai2025/CUG_LogoImage";
+
+export default function AnkundigungCUG() {
+  return (
+    <>
+      <h2>🍀 Es stellt sich vor: Cosplay Union Germany! 🍀</h2>
+
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          gap: "20px",
+          marginBottom: "20px",
+        }}
+      >
+        <DynamicContent
+          $widthpercent={40} // soll 40% von der Seite einnehmen
+          //$maxwidth={300}
+          $align="center"
+          $justify="center"
+        >
+          <Image
+            src={CUG_LogoImage}
+            alt="Cosplay Union Germany Logo" // Wird zB vom Vorlesetool vorgelesen -> immer Text
+            style={{
+              width: "100%",
+              height: "auto",
+              borderRadius: "10px",
+            }}
+          />
+        </DynamicContent>
+        <DynamicContent $widthpercent={60}>
+          {/* Muss mit oberer übereinstimmen */}
+          <p>
+            Cosplay Union Germany ist eine Vereinigung von Fans, die versuchen,
+            originalgetreue Kostüme aus dem Science-Fiction oder
+            Fantasy-Universum zu bauen, egal aus welchem Genre.
+            <br />
+            <br />
+            Das Ziel von Cosplay Union ist es, mit authentischen Kostümen auf
+            verschiedenen Veranstaltungen ein Highlight zu sein und für große
+            Augen und Begeisterung bei kleinen und großen Fans zu sorgen.
+          </p>
+        </DynamicContent>
+      </div>
+    </>
+  );
+}
