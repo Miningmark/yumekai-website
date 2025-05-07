0f144156fa9f35a200b3f840d2ee934b7eb76d02
Markus <markus.sibbe@t-online.de>
2025-05-07 13:13:38 +0200

    fix 4

--- src/components/articles/2025/Programm2025.js
+++ src/components/articles/2025/Programm2025.js
@@ -13,24 +13,21 @@ const DisabledButton = styled(StyledButton)`
 
 export default function Programm2025() {
   return (
-    <>
-        <h2>YumeKai 2025</h2>
+<> 
+    <h2>YumeKai 2025</h2>
 
-        <p>Das vollständige Programm der YumeKai wird es hier ab dem 17.05.2025 zu sehen geben.</p>
-        <div>
-            <DisabledButton>zum Programm</DisabledButton>
-        </div>
-        
+    <p>Das vollständige Programm der YumeKai wird ab dem 17.05.2025 hier verfügbar sein.</p>
+    <div>
+        <DisabledButton>Zum Programm</DisabledButton>
+    </div>
 
-        <h3>Öffnungszeiten</h3>
-        <p>
-            <strong>Samstag:</strong> 10:00 - 21:00 Uhr
-            <br/>
-            <strong>Sonntag:</strong> 10:00 - 17:30 Uhr
-            <br/>
-            <strong>Cosplayball:</strong> Samstag 19:00 - 23:00 Uhr
-            <br/>
-        </p>
-    </>
+    <h3>Öffnungszeiten</h3>
+    <p>
+        <strong>Samstag:</strong> 10:00 - 21:00 Uhr <br/>
+        <strong>Sonntag:</strong> 10:00 - 17:30 Uhr <br/>
+        Goldticket-Besitzer dürfen an beiden Tagen bereits um 09:30 Uhr die YumeKai betreten. <br/>
+        <strong>Cosplayball:</strong> Samstag 19:00 - 23:00 Uhr
+    </p>
+</>
   )
 }
