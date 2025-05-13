0d6ea857219462ca79dabd701f60284c9b5b5cdf
Markus <markus.sibbe@t-online.de>
2025-05-13 09:37:43 +0200

    add allgemein page

---
+++ src/pages/programm2025/allgemein.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+import styled from "styled-components";
+import { useRef, useEffect, useState } from "react";
+
+//Components
+import Columns2 from "@/components/elements/Columns2";
+import Columns3 from "@/components/elements/Columns3";
+import RectangleContainer from "@/components/elements/RectangleContainer";
+import MovingContentWrapper from "@/components/elements/MovingContent";
+import { SpacerEmpty, StyledLink } from "@/components/styledComponents";
+import ReturnButton from "@/components/menu/ReturnButton";
+import ContentCard from "@/components/elements/ContentCard";
+
+//Images
+
+
+
+export default function Allgemein(){
+    return(
+    <>
+        <h1>Allgemein</h1>
+    </>
+    )
+}
