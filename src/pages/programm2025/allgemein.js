d98e24ff2ffeccd55166479bae35074a9f922109
Markus <markus.sibbe@t-online.de>
2025-05-13 13:25:02 +0200

    add map

--- src/pages/programm2025/allgemein.js
+++ src/pages/programm2025/allgemein.js
@@ -11,17 +11,34 @@ import MovingContentWrapper from "@/components/elements/MovingContent";
 import { SpacerEmpty, StyledLink } from "@/components/styledComponents";
 import ReturnButton from "@/components/menu/ReturnButton";
 import ContentCard from "@/components/elements/ContentCard";
+import { StyledLinkAsButton } from "@/components/elements/StyledLinkAsButton";
 
 //Images
-
+import mapImage from "/public/assets/images/yumekai2025/map.png";
 
 
 export default function Allgemein(){
+
+
     return(
     <>
         <h1>Allgemein</h1>
 
         <p>Text......</p>
+
+        <Image
+                  src={mapImage}
+                  alt="Anfahrt Karte"
+                  style={{
+                    width: "100%",
+                    height: "auto",
+                    borderRadius: "10px",
+                  }}
+                />
+                <small>Hintergrundkarte: © Bayerische Vermessungsverwaltung (2025), Datenquelle: Geoportal Bayern www.geoportal.bayern.de</small>
+
+         <StyledLinkAsButton href={"https://maps.app.goo.gl/o7RvbkgHpFvpPAjZ7"}>zu Google Maps</StyledLinkAsButton>
+
     </>
     )
 }
