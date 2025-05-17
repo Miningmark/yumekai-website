a8daa50aee9d8ca28b74ad136fc9ee11b7060e1f
Markus <markus.sibbe@t-online.de>
2025-05-17 13:52:06 +0200

    chnage return button

--- src/components/menu/ReturnButton.js
+++ src/components/menu/ReturnButton.js
@@ -6,9 +6,9 @@ import styled from "styled-components";
 import IconReturn from "/public/assets/icons/chevron_left.svg";
 
 const LinkButton = styled(Link)`
-  position: fixed;
-  left: 50px;
-  top: 100px;
+  position: sticky;
+  left: 0px;
+  top: 80px;
   z-index: 9999;
   width: 40px;
   height: 40px;
@@ -22,9 +22,18 @@ const LinkButton = styled(Link)`
   transition: 0.3s;
   z-index: 900;
   opacity: 0.8;
+  margin-top: 10px;
 
   text-decoration: none;
   color: ${({ theme }) => theme.primaryColor};
+
+  @media (max-width: 900px) {
+    left: 5px;
+    top: 80px;
+
+    width: 35px;
+    height: 35px;
+  }
 
   svg {
     fill: black;
