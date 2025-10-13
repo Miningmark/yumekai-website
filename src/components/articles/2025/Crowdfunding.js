64e4eec0d69bb0d09d8abfd5934ded41d0b8566b
Markus <markus.sibbe@t-online.de>
2025-10-13 11:59:59 +0200

    add new Hiru Image

--- src/components/articles/2025/Crowdfunding.js
+++ src/components/articles/2025/Crowdfunding.js
@@ -3,12 +3,12 @@ import {DynamicContent } from "@/components/styledComponents";
 import { StyledLink } from "@/components/styledComponents";
 
 //Images
-import HiruImage from "/public/assets/hirus/Hiru.png";
+import HiruImage from "/public/assets/hirus/Hiru_Support_25.png";
 
 export default function Crowdfunding() {
   return (
     <>
-      <h2></h2>
+      <h2>YumeKai-Crowdfunding</h2>
 
       <div
         style={{
@@ -40,11 +40,11 @@ export default function Crowdfunding() {
           {/* Muss mit oberer übereinstimmen */}
           <h3>Hilf mit, die YumeKai 2026 Wirklichkeit werden zu lassen!</h3>
          <p>Die YumeKai 2026 braucht deine Unterstützung! Wir sind auf einem spannenden Crowdfunding-Trip, um dieses einzigartige Event zu realisieren.
-        <br/>
+        <br/><br/>
             Bis zum 31. Dezember 2025 kannst du uns auf  <StyledLink href="/shop" target="_blank">
               YumeKai-Crowdfunding
-            </StyledLink> unterstützen und dafür tolle Belohnungen
-            <br/>
+            </StyledLink> unterstützen und dafür tolle Belohnungen.
+            <br/><br/>
             Jede Spende, egal wie klein, bringt uns näher an unser Ziel. Werde Teil der YumeKai-Community und hilf uns, ein unvergessliches Erlebnis zu schaffen!</p>
 
         </DynamicContent>
